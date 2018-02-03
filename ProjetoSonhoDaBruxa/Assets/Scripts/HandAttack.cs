a3b0a1cee0398c301410aceb86329707c08122ee
Michelle Beadle <michellesantiagobeadle@hotmail.com>
2018-02-03 16:02:04 -0200

    ataque da mao

--- ProjetoSonhoDaBruxa/Assets/Scripts/HandAttack.cs
+++ ProjetoSonhoDaBruxa/Assets/Scripts/HandAttack.cs
@@ -1,16 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HandAttack : MonoBehaviour {
 
+	private bool _attacking;
+
 	// Use this for initialization
 	void Start () {
-		
+		StartCoroutine (HandAttacking());
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Q)) {
+			
+			//gameObject.GetComponent<BoxCollider2D> ().transform.localRotation = new Vector3 (0, 0, 180f);
+		}
 		
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+
+		if (other.gameObject.tag == "Player") {
+			//SceneManager.LoadScene("GameOver");
+			Debug.Log("Perdeu");
+		}
+	}
+
+	IEnumerator HandAttacking() {
+
+		while(true){
+			
+			//trigger animation
+			if (_attacking) {
+				gameObject.transform.Rotate (0, 0, 90);
+				_attacking = false;
+			} else {
+				gameObject.transform.Rotate (0, 0, -90);
+				_attacking = true;
+			}
+
+			yield return new WaitForSeconds(1.0f);
+		}
 	}
 }
