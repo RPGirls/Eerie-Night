d27cd009f9a9f35dfa23c814fd0faf8979c0ebc9
Michelle Beadle <michellesantiagobeadle@hotmail.com>
2018-02-03 14:51:02 -0200

    Sprite door

---
+++ ProjetoSonhoDaBruxa/Assets/Scripts/WinGame.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinGame : MonoBehaviour {
+
+	public string WinLevel;
+
+	void OnTriggerEnter2D(Collider2D other) {
+
+		if (other.tag == "Player") {
+			//SceneManager.LoadScene (WinLevel);
+			Debug.Log ("Ganhou");
+		}
+	}
+
+	void OnTriggerStay2D(Collider2D other) {
+
+
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+
+
+	}
+
+
+}
