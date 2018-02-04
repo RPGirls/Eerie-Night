fa01950c9f345b57e8c43a4a3d41cd42bb68abda
GiuDuncan <giuliaduncan@gmail.com>
2018-02-04 11:52:27 -0200

    break object animation

--- ProjetoSonhoDaBruxa/Assets/Scripts/BreakableObject.cs
+++ ProjetoSonhoDaBruxa/Assets/Scripts/BreakableObject.cs
@@ -2,12 +2,21 @@
 
 namespace Assets.Scripts
 {
+    [RequireComponent(typeof(Animator))]
     public class BreakableObject : MonoBehaviour {
 
         public Sprite BrokenObject;
+        private Animator _anim;
+
+        public void Start()
+        {
+            _anim = GetComponent<Animator>();
+        }
 
         public void BreakObject()
         {
+            if(_anim != null)
+                _anim.SetBool("break", true);
             GetComponent<SpriteRenderer>().sprite = BrokenObject;
             tag = "Untagged";
         }
