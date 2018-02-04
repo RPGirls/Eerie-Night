648d1eecb1acef69be160d2eb4b847d257e33ae5
Michelle Beadle <michellesantiagobeadle@hotmail.com>
2018-02-03 23:17:23 -0200

    sem erro

--- ProjetoSonhoDaBruxa/Assets/Scripts/BreakObjects.cs
+++ ProjetoSonhoDaBruxa/Assets/Scripts/BreakObjects.cs
@@ -69,6 +69,7 @@ namespace Assets.Scripts
             _ready = false;
             BreakObject(col);
             _playerControl.IsPulling = false;
+			_playerControl.ApplyForce ();
         }
 
         private void StopParticle()
