3656c027cd824637321c21b0ad9fab9322efbb77
Bruna Lucchesi <brunalvillela@gmail.com>
2018-03-05 22:18:47 -0300

    audiomanager

---
+++ ProjetoSonhoDaBruxa/Assets/Scripts/SoundFade.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    public class SoundFade : MonoBehaviour {
+
+        [SerializeField]
+        private float delay = 1f;
+
+        public IEnumerator FadeOutMusic()
+        {
+            float elapsedTime = 0;
+            float currentVolume = GetComponent<AudioSource>().volume;
+
+            while (elapsedTime < delay)
+            {
+                elapsedTime += Time.deltaTime;
+                GetComponent<AudioSource>().volume = Mathf.Lerp(currentVolume, 0, elapsedTime / delay);
+                yield return null;
+            }
+
+            GetComponent<AudioSource>().mute = true;
+        }
+    }
+}
